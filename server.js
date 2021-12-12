1386c68a3bb809862527297b96849d125fc73d9b
Justin B. Watson <justin@brimelive.com>
2021-12-12 08:55:02 -0600

    add controls

--- server.js
+++ server.js
@@ -4,9 +4,14 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+require('dotenv').config()
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/controls', (req, res) => {
+  res.sendFile(__dirname + '/controls.html');
 });
 
 io.on('connection', (socket) => {
@@ -14,7 +19,16 @@ io.on('connection', (socket) => {
     socket.on('disconnect', () => {
         console.log('client disconnected');
       });
+      socket.on('play', function(msg) {
+        io.emit('play'); // Send the play event to the client
+      });
+      socket.on('pause', function(msg) {
+        io.emit('pause'); // Send the pause event to the client
+      });
+      socket.on('skip', function(msg) {
+        io.emit('skip'); // Send the skip event to the client
   });
+});
 
   // API ENDPOINTS
   app.get('/play', (req, res) => {
@@ -22,11 +36,11 @@ io.on('connection', (socket) => {
     res.send({result: "ok", event: "play"})
   });
   app.get('/pause', (req, res) => {
-    io.emit('pause'); // Send the play event to the client
+    io.emit('pause'); // Send the pause event to the client
     res.send({result: "ok", event: "pause"})
   });
   app.get('/skip', (req, res) => {
-    io.emit('skip'); // Send the play event to the client
+    io.emit('skip'); // Send the skip event to the client
     res.send({result: "ok", event: "skip"})
   });     
   app.get('/load', (req, res) => {
